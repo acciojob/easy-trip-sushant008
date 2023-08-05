f1c867817ebacd0a5ec3933ed0ce9f572f216970
sushant008 <129641196+sushant008@users.noreply.github.com>
2023-08-05 23:02:08 +0530

    easy m

--- src/main/java/com/driver/EasyMyTrip.java
+++ src/main/java/com/driver/EasyMyTrip.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-public class EaseMyTrip {
+class EaseMyTrip {
 
 	public static void main(String[] args) {
 
